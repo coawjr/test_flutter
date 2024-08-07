7b8426fc4ceb86e73fbe93ff04d388e5c2f1ce29
coawjr <132333549+coawjr@users.noreply.github.com>
2024-08-08 03:03:24 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -32,41 +32,37 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  List Alphabet = ['A', 'B', 'C', 'D', 'E'];
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
+ 
     return Scaffold(
+
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
-      
       ),
-      body: Expanded(
-        
-        child: ListView.builder(
-          itemCount: Alphabet.length + 1,
-          itemBuilder: (context, index) {
-            if (index == 0) {
-              return Text('List of Alphabets');
-              
-            }
-        return Card(
-          child: Text(Alphabet[index-1]),
-        );
-          },
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
-      ),
+      ),        
+
 
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
