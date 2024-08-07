24c175b1f7d6db3093976e280900589ce2cd2a46
coawjr <132333549+coawjr@users.noreply.github.com>
2024-08-08 03:29:57 +0900

    test_state

--- lib/main.dart
+++ lib/main.dart
@@ -32,10 +32,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _counter2 = 1;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+    });
+  }
+
+    void _twice() {
+    setState(() {
+      _counter2 = _counter2 * 2;
     });
   }
 
@@ -53,22 +60,38 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              '足し算:',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+             const Text(
+              '2倍:',
+            ),
+            Text(
+              '$_counter2',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
           ],
         ),
       ),        
-
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+           FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: _twice,
+            tooltip: 'twice',
+            child: const Icon(Icons.close),
+          ),
+        ],
       ), 
+      
     );
   }
 }
